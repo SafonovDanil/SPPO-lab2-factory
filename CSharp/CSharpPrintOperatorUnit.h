48f3a22cb1beb2e55b4761cda7b83d2971a0b1c0
Danil Safonov <samiyleviymail@gmai.com>
2021-04-26 17:47:50 +0700

    Изменены Print методы для C# и Java

--- CSharp/CSharpPrintOperatorUnit.h
+++ CSharp/CSharpPrintOperatorUnit.h
@@ -8,8 +8,7 @@ public:
  explicit CSharpPrintOperatorUnit( const std::string& text ) :
 m_text( text ) { }
  std::string compile( unsigned int level = 0 ) const {
- return generateShift( level ) + "printf( \"" + m_text
-+ "\" );\n";
+ return generateShift( level ) + "Colsole.WriteLine(" + m_text + ")\n;";
  }
 private:
  std::string m_text;
--- Java/JavaPrintOperatorUnit.h
+++ Java/JavaPrintOperatorUnit.h
@@ -8,7 +8,7 @@ public:
  explicit JavaPrintOperatorUnit( const std::string& text ) :
 m_text( text ) { }
  std::string compile( unsigned int level = 0 ) const {
- return generateShift( level ) + "printf( \"" + m_text
+ return generateShift( level ) + "System.out.print(" + m_text
 + "\" );\n";
  }
 private:
